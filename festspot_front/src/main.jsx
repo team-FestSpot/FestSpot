b0109e93566ed0b36c14f18bb4625c9968eb7a32
szversion <ghtjq102099@naver.com>
2025-08-04 16:50:36 +0900

    merge conflict solve

--- festspot_front/src/main.jsx
+++ festspot_front/src/main.jsx
@@ -1,19 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60,
-      retry: 0,
-    },
-  },
-});
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
+  <StrictMode>
     <App />
-    {/* <DashBoard /> */}
-  </QueryClientProvider>
+  </StrictMode>
 );
