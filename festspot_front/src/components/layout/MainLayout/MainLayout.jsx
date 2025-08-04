16a6bf1511d340449883465661c1a630f830ac13
szversion <ghtjq102099@naver.com>
2025-08-04 17:42:28 +0900

    rootrouter 세팅, rootlayout 세팅 이미지 엑박 해결해야 함

---
+++ festspot_front/src/components/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import UpperSideBar from "../../../SideBar/UpperSideBar/UpperSideBar";
+import * as s from "./styles";
+import React from "react";
+
+function MainLayout({ children }) {
+  return (
+    <div css={s.layout}>
+      <UpperSideBar />
+      {children}
+    </div>
+  );
+}
+
+export default MainLayout;
---
+++ festspot_front/src/routes/AuthRouter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../page/Auth/Login/Login";
+import SignUp from "../page/Auth/SignUp/SignUp";
+
+function AuthRouter(props) {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+}
+
+export default AuthRouter;
