012c2509dfafc0a3afec3e935a662f01f26612db
szversion <ghtjq102099@naver.com>
2025-08-04 17:42:28 +0900

    rootrouter 세팅, rootlayout 세팅 이미지 엑박 해결해야 함

---
+++ festspot_front/src/routes/RooteRouter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DashBoard from "../admin/page/DashBoard/DashBoard";
+import AuthRouter from "./AuthRouter";
+import MainLayout from "../components/layout/MainLayout/MainLayout";
+
+function RooteRouter(props) {
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/auth/*" element={<AuthRouter />} />
+        <Route path="/" element={<DashBoard />} />
+      </Routes>
+    </MainLayout>
+  );
+}
+
+export default RooteRouter;
