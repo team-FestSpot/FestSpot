75469886d19950ee2c74c364fa9d5c41bb34b667
Su1226 <br12232620@gmail.com>
2025-08-04 16:43:28 +0900

    conflict merge complete

--- festspot_front/src/main.jsx
+++ festspot_front/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Login from "./page/Auth/Login/Login.jsx";
-import SignUp from "./page/Auth/SignUp/SignUp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
