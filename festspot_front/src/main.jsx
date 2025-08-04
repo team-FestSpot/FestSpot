a8285d1f3315fc6019397865d88ee28c03c25073
Su1226 <br12232620@gmail.com>
2025-08-04 16:38:44 +0900

    lsw: Make Front PAGE - Login, SignUp

--- festspot_front/src/main.jsx
+++ festspot_front/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./page/Auth/Login/Login.jsx";
+import SignUp from "./page/Auth/SignUp/SignUp.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    {/* <App /> */}
+    <Login />
+    {/* <SignUp /> */}
+  </>
+);
