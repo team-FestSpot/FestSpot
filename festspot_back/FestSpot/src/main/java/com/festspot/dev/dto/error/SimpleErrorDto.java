ffb61594e34ac6057860d040fc8ec2bd22171364
Su1226 <br12232620@gmail.com>
2025-08-06 12:37:49 +0900

    lsw : Back Login - Test

---
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/dto/error/SimpleErrorDto.java
@@ -0,0 +1,11 @@
+package com.festspot.dev.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SimpleErrorDto {
+    private String title;
+    private String errorMessage;
+}
