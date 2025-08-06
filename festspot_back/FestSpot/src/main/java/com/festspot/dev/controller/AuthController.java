6ff0258da78ddac3acb5cd3ca9e414dc75e38818
Su1226 <br12232620@gmail.com>
2025-08-06 11:43:28 +0900

    lsw : Back Sign Up v1

---
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.festspot.dev.controller;
+
+import com.festspot.dev.dto.auth.UserSignUpDto;
+import com.festspot.dev.dto.reponse.ResponseDto;
+import com.festspot.dev.service.AuthService;
+import com.festspot.dev.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> singup(@RequestBody @Valid UserSignUpDto dto) throws BindException {
+        return ResponseEntity.ok(ResponseDto.success(authService.signUp(dto)));
+
+    }
+}
---
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/dto/auth/UserLoginDto.java
@@ -0,0 +1,9 @@
+package com.festspot.dev.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class UserLoginDto {
+    private String userLoginId;
+    private String userPassword;
+}
