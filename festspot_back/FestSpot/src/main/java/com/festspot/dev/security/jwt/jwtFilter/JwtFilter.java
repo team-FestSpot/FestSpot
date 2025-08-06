080d2e7ad7d4125d69d01e8f5a0d6e3c306264b0
szversion <ghtjq102099@naver.com>
2025-08-06 11:55:08 +0900

    머지 충돌 해결

--- festspot_back/FestSpot/src/main/java/com/festspot/dev/security/jwt/jwtFilter/JwtFilter.java
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/security/jwt/jwtFilter/JwtFilter.java
@@ -1,4 +1,68 @@
 package com.festspot.dev.security.jwt.jwtFilter;
 
-public class JwtFilter {
+import com.festspot.dev.domain.user.User;
+import com.festspot.dev.domain.user.UserMapper;
+import com.festspot.dev.security.jwt.JwtUtil;
+import com.festspot.dev.security.jwt.model.PrincipalUser;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter implements Filter {
+
+  private final JwtUtil jwtUtil;
+  private final UserMapper userMapper;
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    boolean isOption = request.getMethod().equalsIgnoreCase("OPTION");
+    if (isOption) {
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
+
+    String authorization = request.getHeader("Authorization");
+    authenticate(authorization);
+
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+
+  private void authenticate(String token) {
+    String validatedToken = jwtUtil.validateBearerToken(token);
+    if (validatedToken == null) {
+      return;
+    }
+    Claims claims = jwtUtil.getClaims(validatedToken);
+    if (claims == null) {
+      return;
+    }
+    setAuthentication(claims);
+  }
+
+  private void setAuthentication(Claims claims) {
+    Integer userId = (Integer) claims.get("userId");
+    User foundUser = userMapper.findByUserId(userId);
+    if (foundUser == null) {
+      return;
+    }
+    PrincipalUser principal = PrincipalUser.builder().user(foundUser).build();
+    Authentication authentication = new UsernamePasswordAuthenticationToken(SecurityContextHolder.getContext().getAuthentication().getPrincipal(), "",
+        principal.getAuthorities());
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+  }
+
 }
