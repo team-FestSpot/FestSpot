62e2dd77f6f442f9b714aefc57de4bf45ec93c6e
Su1226 <br12232620@gmail.com>
2025-08-08 17:30:49 +0900

    lsw - conflict resolve

--- festspot_front/src/components/layout/MainLayout/MainLayout.jsx
+++ festspot_front/src/components/layout/MainLayout/MainLayout.jsx
@@ -1,27 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import { BoardContext } from "../../../constants/BoardContext";
-import PostSideBar from "../../../SideBar/PostSideBar/PostSideBar";
 import UpperSideBar from "../../../SideBar/UpperSideBar/UpperSideBar";
 import * as s from "./styles";
-import React, { useState } from "react";
 
 function MainLayout({ children }) {
-  const [currentBoard, setCurrentBoard] = useState('free');
-  const [loading, setLoading] = useState(false);
-
-  const handleBoardOnChange = (boardId) => {
-    console.log('MainLayout: 게시판 변경됨 ->', boardId);
-    setCurrentBoard(boardId);
-  };
-
   return (
-    <BoardContext.Provider value={{ currentBoard, onBoardChange: handleBoardOnChange, loading }}>
-      <div css={s.layout}>
-        <UpperSideBar />
-        <PostSideBar currentBoard={currentBoard} onBoardChange={handleBoardOnChange} loading={loading}/>
-        {children}
+    <div css={s.layout}>
+      <UpperSideBar />
+      <div css={s.container}>
+        <div css={s.children}>{children}</div>
       </div>
-    </BoardContext.Provider>
+    </div>
   );
 }
 
