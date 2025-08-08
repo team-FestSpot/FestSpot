746436f5de51c24fbeb1fd8b25378b8d515d6952
Su1226 <br12232620@gmail.com>
2025-08-08 17:24:02 +0900

    lsw : Front - post doing

--- festspot_front/src/components/layout/MainLayout/MainLayout.jsx
+++ festspot_front/src/components/layout/MainLayout/MainLayout.jsx
@@ -1,15 +1,27 @@
 /** @jsxImportSource @emotion/react */
+import { BoardContext } from "../../../constants/BoardContext";
 import PostSideBar from "../../../SideBar/PostSideBar/PostSideBar";
 import UpperSideBar from "../../../SideBar/UpperSideBar/UpperSideBar";
 import * as s from "./styles";
-import React from "react";
+import React, { useState } from "react";
 
 function MainLayout({ children }) {
+  const [currentBoard, setCurrentBoard] = useState('free');
+  const [loading, setLoading] = useState(false);
+
+  const handleBoardOnChange = (boardId) => {
+    console.log('MainLayout: 게시판 변경됨 ->', boardId);
+    setCurrentBoard(boardId);
+  };
+
   return (
-    <div css={s.layout}>
-      <UpperSideBar />
-      {children}
-    </div>
+    <BoardContext.Provider value={{ currentBoard, onBoardChange: handleBoardOnChange, loading }}>
+      <div css={s.layout}>
+        <UpperSideBar />
+        <PostSideBar currentBoard={currentBoard} onBoardChange={handleBoardOnChange} loading={loading}/>
+        {children}
+      </div>
+    </BoardContext.Provider>
   );
 }
 
--- festspot_front/src/components/layout/MainLayout/styles.js
+++ festspot_front/src/components/layout/MainLayout/styles.js
@@ -2,6 +2,7 @@ import { css } from "@emotion/react";
 
 export const layout = css`
   display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
 `;
