d22950a213cccd29688490e4fdcbc29bded0776c
szversion <ghtjq102099@naver.com>
2025-08-18 08:23:12 +0900

    tippy 안에 tippy 넣는 중

--- festspot_front/src/components/layout/MainLayout/MainLayout.jsx
+++ festspot_front/src/components/layout/MainLayout/MainLayout.jsx
@@ -6,20 +6,28 @@ import * as s from "./styles";
 import React, { useState } from "react";
 
 function MainLayout({ children }) {
-  const [currentBoard, setCurrentBoard] = useState('free');
+  const [currentBoard, setCurrentBoard] = useState("free");
   const [loading, setLoading] = useState(false);
 
   const handleBoardOnChange = (boardId) => {
-    console.log('MainLayout: 게시판 변경됨 ->', boardId);
+    console.log("MainLayout: 게시판 변경됨 ->", boardId);
     setCurrentBoard(boardId);
   };
 
   return (
-    <BoardContext.Provider value={{ currentBoard, onBoardChange: handleBoardOnChange, loading }}>
+    <BoardContext.Provider
+      value={{ currentBoard, onBoardChange: handleBoardOnChange, loading }}
+    >
       <div css={s.layout}>
         <UpperSideBar />
-        <PostSideBar currentBoard={currentBoard} onBoardChange={handleBoardOnChange} loading={loading}/>
-        {children}
+        <PostSideBar
+          currentBoard={currentBoard}
+          onBoardChange={handleBoardOnChange}
+          loading={loading}
+        />
+        <div css={s.container}>
+          <div css={s.children}>{children}</div>
+        </div>
       </div>
     </BoardContext.Provider>
   );
