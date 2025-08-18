c736d00728aa6e744f9bbf0da4193e0a8da4ae44
untilthe-end <letsgohangun@gmail.com>
2025-08-18 12:38:16 +0900

    백엔드 entity 수정 완료

--- festspot_back/FestSpot/src/main/java/com/festspot/dev/service/AdminService.java
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/service/AdminService.java
@@ -33,8 +33,8 @@ public class AdminService {
     }
 
     PerformanceRegion performanceRegion = performanceRegionMapper.findByRegionName(
-        dto.getPrfstate());
-    PerformanceState performanceState = performanceStateMapper.findByState(dto.getArea());
+        dto.getArea());
+    PerformanceState performanceState = performanceStateMapper.findByState(dto.getPrfstate());
 
     Performance performance = dto.toEntity(performanceRegion, performanceState);
     int performanceInsert = performanceMapper.insert(performance);
@@ -53,8 +53,8 @@ public class AdminService {
 
     List<Performance> performanceList = dtos.stream().map(dto -> {
       PerformanceRegion performanceRegion = performanceRegionMapper.findByRegionName(
-          dto.getPrfstate());
-      PerformanceState performanceState = performanceStateMapper.findByState(dto.getArea());
+          dto.getArea());
+      PerformanceState performanceState = performanceStateMapper.findByState(dto.getPrfstate());
       return dto.toEntity(performanceRegion, performanceState);
     }).toList();
     int result = performanceMapper.insertMany(performanceList);
