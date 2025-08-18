aeb53e8acafd83365e3dcd6c3a1b650292dc5cab
untilthe-end <letsgohangun@gmail.com>
2025-08-18 12:38:16 +0900

    백엔드 entity 수정 완료

--- festspot_front/src/routes/RooteRouter.jsx
+++ festspot_front/src/routes/RooteRouter.jsx
@@ -3,9 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import AuthRouter from "./AuthRouter";
 import MainLayout from "../components/layout/MainLayout/MainLayout";
 import Home from "../page/Home/Home";
-import DashBoard from "../page/admin/DashBoard/DashBoard";
 import FestivalBoard from "../page/Auth/Board/FestivalBoard";
-import PostWrite from "../page/Auth/post/PostWrite";
+import AdminMainPage from "../page/admin/AdminMainPage/AdminMainPage";
 
 function RooteRouter(props) {
   return (
@@ -13,7 +12,7 @@ function RooteRouter(props) {
       <Routes>
         <Route path="/auth/*" element={<AuthRouter />} />
         <Route path="/board" element={<FestivalBoard />} />
-        <Route path="/board/write" element={<PostWrite />} />
+        <Route path="/admin" element={<AdminMainPage />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </MainLayout>
