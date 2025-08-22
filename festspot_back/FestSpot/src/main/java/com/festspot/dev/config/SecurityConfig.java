b8574411fbc1e3dbae17d183460f023c3ea2ec73
untilthe-end <letsgohangun@gmail.com>
2025-08-22 09:31:17 +0900

    집에서 한거 머지중

--- festspot_back/FestSpot/src/main/java/com/festspot/dev/config/SecurityConfig.java
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/config/SecurityConfig.java
@@ -47,15 +47,15 @@ public class SecurityConfig {
 
     http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-        // 모든 요청 허용
-        http.authorizeHttpRequests(auth -> {
+    // 모든 요청 허용
+    http.authorizeHttpRequests(auth -> {
           auth.requestMatchers("/oauth2/**").permitAll();
             auth.requestMatchers("/login/oauth2/code/**").permitAll();
             auth.requestMatchers("/api/auth/**").permitAll();
             auth.requestMatchers("/api/board/**").permitAll();
             auth.requestMatchers("/image/**").permitAll();
             auth.anyRequest().authenticated();
-        });
+    });
 
     http.exceptionHandling(
         handling -> handling.authenticationEntryPoint((request, response, authException) -> {
--- festspot_back/FestSpot/src/main/java/com/festspot/dev/domain/userRole/UserRoleMapper.java
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/domain/userRole/UserRoleMapper.java
@@ -1,4 +1,14 @@
 package com.festspot.dev.domain.userRole;
 
-public interface userRoleMapper {
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserRoleMapper {
+
+  int insert(UserRole userRole);
+
+  int deleteByUserId(Integer userId);
+
+  int deleteByUserIds(List<Integer> userIds);
 }
