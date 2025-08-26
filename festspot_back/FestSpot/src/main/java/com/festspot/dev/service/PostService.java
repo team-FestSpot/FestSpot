e1405055348e7a9aee34572ef4962796e0b5966b
Su1226 <br12232620@gmail.com>
2025-08-26 12:36:52 +0900

    Post Content Response Success / Console is not response

--- festspot_back/FestSpot/src/main/java/com/festspot/dev/service/PostService.java
+++ festspot_back/FestSpot/src/main/java/com/festspot/dev/service/PostService.java
@@ -1,9 +1,11 @@
 package com.festspot.dev.service;
 
+import com.festspot.dev.domain.post.Post;
 import com.festspot.dev.domain.post.PostMapper;
 import com.festspot.dev.domain.post.PostSearchOption;
 import com.festspot.dev.domain.postCategory.PostCategoryMapper;
 import com.festspot.dev.dto.post.PostsRespDto;
+import com.festspot.dev.security.model.PrincipalUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ public class PostService {
 
   private final PostMapper postMapper;
   private final PostCategoryMapper postCategoryMapper;
+  private final PrincipalUtil principalUtil;
 
   public PostsRespDto getAllPosts(PostSearchOption postSearchOption) {
     return PostsRespDto.builder()
@@ -31,4 +34,10 @@ public class PostService {
         .totalPage((int) Math.ceil(postMapper.countByCategoryId(postSearchOption)))
         .build();
   }
+
+  public Post getPost(Integer postId) {
+    Integer userId = principalUtil.getUserIdOrNull();
+    return postMapper.findByPostId(postId, userId);
+  }
+
 }
