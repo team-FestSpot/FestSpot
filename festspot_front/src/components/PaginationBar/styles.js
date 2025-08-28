92fad098b0b252a1f88db0badc5ebbfb397b3cf1
untilthe-end <letsgohangun@gmail.com>
2025-08-28 17:50:34 +0900

    게시글 가져오기 xml 하는중

---
+++ festspot_front/src/components/PaginationBar/styles.js
@@ -0,0 +1,59 @@
+import { css } from "@emotion/react";
+
+export const paginationBarLayout = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  gap: 0.4rem;
+`;
+
+export const paginationButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  aspect-ratio: 1/1;
+  padding: 0.4rem;
+  box-sizing: border-box;
+  border: 1px solid #e55a42;
+  border-radius: 10%;
+  background-color: white;
+  color: #e55a42;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fcebe8;
+  }
+`;
+
+export const paginationNumContainer = (gap) => css`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  height: 100%;
+  gap: ${gap};
+`;
+
+export const paginationNum = (isCurrentPage) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  aspect-ratio: 1/1;
+  padding: 0.4rem;
+  box-sizing: border-box;
+  border: 1px solid #e55a42;
+  border-radius: 10%;
+  background-color: ${isCurrentPage ? "#e55a42" : "white"};
+  color: ${isCurrentPage ? "white" : "#e55a42"};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fcebe8;
+  }
+`;
