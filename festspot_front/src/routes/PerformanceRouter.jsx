7afd68800be1fd735b9974eda96e062898de953d
lley01 <lle__y01@naver.com>
2025-08-29 17:42:04 +0900

    performance detail information page design(in progress...)

---
+++ festspot_front/src/routes/PerformanceRouter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PerformanceMain from "../page/performance/PerformanceMain/PerformanceMain";
+import Feed from "../page/Feed/Feed";
+
+function PerformanceRouter(props) {
+  return (
+    <PerformanceMain>
+      <Routes>
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </PerformanceMain>
+  );
+}
+
+export default PerformanceRouter;
--- festspot_front/src/routes/UserRouter.jsx
+++ festspot_front/src/routes/UserRouter.jsx
@@ -5,6 +5,7 @@ import Home from "../page/Home/Home";
 import MainLayout from "../components/layout/MainLayout/MainLayout";
 import CommunityBoardRouter from "./CommunityBoardRouter";
 import MyPageRouter from "./MyPageRouter";
+import PerformanceRouter from "./PerformanceRouter";
 
 function UserRouter(props) {
   return (
@@ -13,6 +14,7 @@ function UserRouter(props) {
         <Route path="/auth/*" element={<AuthRouter />} />
         <Route path="/board/*" element={<CommunityBoardRouter />} />
         <Route path="/mypage/*" element={<MyPageRouter />} />
+        <Route path="/performance/*" element={<PerformanceRouter />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </MainLayout>
